ea7b81b653c6af6591aafc12c2e76391c076ae0c
ghjang <jmxexpert@hotmail.com>
2024-01-31 19:53:17 +0900

    AutoTimerTrigger, ForwardOnlyTrigger 구현 재작성

--- src/modules/strategy/animation/transition/Trigger.js
+++ src/modules/strategy/animation/transition/Trigger.js
@@ -1,37 +1,68 @@
 class AutoTimerTrigger {
+    #resolveWait = null;
+    #rejectWait = null;
+    #timerId = null;
+
     async wait(delay = 0) {
-        return new Promise(resolve => {
-            setTimeout(() => resolve(), delay);
+        return new Promise((resolve, reject) => {
+            this.#resolveWait = resolve;
+            this.#rejectWait = reject;
+            this.#timerId = setTimeout(() => {
+                this.#resolveWait();
+                this.#timerId = null;
+                this.#resolveWait = null;
+                this.#rejectWait = null;
+            }, delay);
         });
+    }
+
+    async stop() {
+        if (this.#timerId) {
+            clearTimeout(this.#timerId);
+            this.#timerId = null;
+        }
+        if (this.#rejectWait) {
+            this.#rejectWait(new Error('Stopped by user'));
+            this.#rejectWait = null;
+        }
+        if (this.#resolveWait) {
+            this.#resolveWait = null;
+        }
     }
 }
 
 
 class ForwardOnlyTrigger {
-    #onKeydown;
-    #callback;
+    #resolveWait = null;
+    #rejectWait = null;
+    #onKeydown = null;
 
-    wait(callback, _delay, _colIndex) {
-        if (this.#onKeydown == null) {
+    async wait(_delay) {
+        return new Promise((resolve, reject) => {
+            this.#resolveWait = resolve;
+            this.#rejectWait = reject;
             this.#onKeydown = (event) => {
                 if (event.code === 'ArrowRight') {
-                    // NOTE: 여기서 '이벤트 핸들러'를 '애로우 함수'로 정의했기 때문에
-                    //       'this'는 'ForwardOnlyTrigger' 객체를 가리킨다.
-                    if (this.#callback != null && typeof this.#callback === 'function') {
-                        this.#callback();
-                        this.#callback = null;
-                    }
+                    window.removeEventListener('keydown', this.#onKeydown);
+                    this.#resolveWait();
                 }
             };
-
             window.addEventListener('keydown', this.#onKeydown);
-        }
-
-        this.#callback = callback;
+        });
     }
     
-    stop() {
-        window.removeEventListener('keydown', this.#onKeydown);
+    async stop() {
+        if (this.#onKeydown) {
+            window.removeEventListener('keydown', this.#onKeydown);
+            this.#onKeydown = null;
+        }
+        if (this.#rejectWait) {
+            this.#rejectWait(new Error('Stopped by user'));
+            this.#rejectWait = null;
+        }
+        if (this.#resolveWait) {
+            this.#resolveWait = null;
+        }
     }
 }
 
